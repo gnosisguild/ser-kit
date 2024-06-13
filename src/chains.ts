a7f5a58d12a01295d25cdd3f15925e2030e8c803
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-06-13 10:04:14 +0200

    improve data model

---
+++ src/chains.ts
@@ -0,0 +1,9 @@
+export const chains = [
+  { chainId: 1, shortName: 'eth' },
+  { chainId: 100, shortName: 'gno' },
+  { chainId: 11155111, shortName: 'sep' },
+  { chainId: 137, shortName: 'matic' },
+  { chainId: 42161, shortName: 'arb1' },
+  { chainId: 43114, shortName: 'avax' },
+  { chainId: 8453, shortName: 'base' },
+] as const
--- src/types.ts
+++ src/types.ts
@@ -3,59 +3,55 @@ import { chains } from './chains'
 export type ChainId = (typeof chains)[number]['chainId']
 export type ChainShortName = (typeof chains)[number]['shortName']
 
-export type PrefixedAddress = `${ChainShortName}:0x${string}`
+export type PrefixedAddress =
+  | `${ChainShortName}:0x${string}`
+  | `eoa:0x${string}`
 
-export interface ExecutionPath {
-  nodes: ExecutionNode[]
-  edges: ExecutionEdge[]
-}
-
-enum ExecutionNodeType {
+export enum AccountType {
   EOA = 'EOA',
   SAFE = 'SAFE',
   ROLES = 'ROLES',
   DELAY = 'DELAY',
 }
 
-interface EoaExecutionNode {
-  type: ExecutionNodeType.EOA
+interface Eoa {
+  type: AccountType.EOA
   address: `0x${string}`
+  prefixedAddress: PrefixedAddress
 }
 
-interface SafeExecutionNode {
-  type: ExecutionNodeType.SAFE
+interface Safe {
+  type: AccountType.SAFE
   address: `0x${string}`
-
   prefixedAddress: PrefixedAddress
+
   chainId: ChainId
   threshold: number
 }
 
-interface RolesExecutionNode {
-  type: ExecutionNodeType.ROLES
+interface Roles {
+  type: AccountType.ROLES
   address: `0x${string}`
-
   prefixedAddress: PrefixedAddress
+
   chainId: ChainId
   defaultRole: Map<`0x${string}`, string>
   multisend: `0x${string}`[]
 }
 
-interface DelayExecutionNode {
-  type: ExecutionNodeType.DELAY
+interface Delay {
+  type: AccountType.DELAY
   address: `0x${string}`
-
   prefixedAddress: PrefixedAddress
+
   chainId: ChainId
 }
 
-export type ExecutionNode =
-  | EoaExecutionNode
-  | SafeExecutionNode
-  | RolesExecutionNode
-  | DelayExecutionNode
+type Contract = Safe | Roles | Delay
 
-enum ExecutionEdgeType {
+export type Account = Eoa | Contract
+
+export enum ConnectionType {
   /** The source node is an owner of the destination safe */
   OWNS = 'OWNS',
   /** The source node is enabled as a module of the destination node */
@@ -64,19 +60,36 @@ enum ExecutionEdgeType {
   IS_MEMBER = 'IS_MEMBER',
 }
 
-interface OwnsExecutionEdge {
-  type: ExecutionEdgeType.OWNS
+interface OwnsConnection {
+  type: ConnectionType.OWNS
 }
 
-interface IsEnabledExecutionEdge {
-  type: ExecutionEdgeType.IS_ENABLED
+interface IsEnabledConnection {
+  type: ConnectionType.IS_ENABLED
 }
 
-interface IsMemberExecutionEdge {
-  type: ExecutionEdgeType.IS_MEMBER
+interface IsMemberConnection {
+  type: ConnectionType.IS_MEMBER
+  roles: string[]
 }
 
-export type ExecutionEdge =
-  | OwnsExecutionEdge
-  | IsEnabledExecutionEdge
-  | IsMemberExecutionEdge
+export type Connection =
+  | OwnsConnection
+  | IsEnabledConnection
+  | IsMemberConnection
+
+/** An execution route starts with the signing EOA or initiating smart contract account. */
+interface StartingPoint {
+  /** The account that is used as the signer or initiator of the transaction */
+  account: Account
+}
+
+/** An execution route flows along an arbitrary number of contract waypoints ending at the controlled avatar */
+export interface Waypoint {
+  /** A description of the account or module at this waypoint */
+  account: Contract
+  /** A description of how the previous waypoint is connected to this contract */
+  connection: Connection
+}
+
+export type Route = [StartingPoint, ...Waypoint[]]
