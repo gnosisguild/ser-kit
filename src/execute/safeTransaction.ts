2ed336cbad514bfd3072fa47ad53fed8be0fe4b2
Cristóvão <cristovao.honorato@gmail.com>
2025-01-08 18:06:46 +0100

    feat: new options for Safe transaction `nonce`: override and enqueue (#28)
    This PR introduces two special modes for nonce config per safe: override
    and enqueue
    
    Note: depends on another PR to be merged first
    
    closes #24

--- src/execute/safeTransaction.ts
+++ src/execute/safeTransaction.ts
@@ -1,3 +1,4 @@
+import assert from 'assert'
 import {
   Address,
   encodeFunctionData,
@@ -5,10 +6,11 @@ import {
   parseAbi,
   zeroAddress,
 } from 'viem'
+import SafeApiKit from '@safe-global/api-kit'
 import { OperationType } from '@safe-global/types-kit'
 
 import { formatPrefixedAddress } from '../addresses'
-import { getEip1193Provider, Options } from './options'
+import { getEip1193Provider, nonceConfig, Options } from './options'
 
 import {
   ChainId,
@@ -28,8 +30,6 @@ export async function prepareSafeTransaction({
   transaction: MetaTransactionRequest
   options?: Options
 }): Promise<SafeTransactionRequest> {
-  const provider = getEip1193Provider({ chainId, options })
-
   const key1 = formatPrefixedAddress(chainId, safe)
   const key2 = key1.toLowerCase() as PrefixedAddress
 
@@ -37,9 +37,54 @@ export async function prepareSafeTransaction({
     options?.safeTransactionProperties?.[key1] ||
     options?.safeTransactionProperties?.[key2]
 
+  return {
+    to: transaction.to,
+    value: transaction.value,
+    data: transaction.data,
+    operation: transaction.operation ?? OperationType.Call,
+    safeTxGas: BigInt(defaults?.safeTxGas || 0),
+    baseGas: BigInt(defaults?.baseGas || 0),
+    gasPrice: BigInt(defaults?.gasPrice || 0),
+    gasToken: getAddress(defaults?.gasToken || zeroAddress),
+    refundReceiver: getAddress(defaults?.refundReceiver || zeroAddress),
+    nonce: await nonce({ chainId, safe, options }),
+  }
+}
+
+async function nonce({
+  chainId,
+  safe,
+  options,
+}: {
+  chainId: ChainId
+  safe: Address
+  options?: Options
+}): Promise<number> {
+  const config = nonceConfig({ chainId, safe, options })
+  if (config == 'enqueue') {
+    return fetchQueueNonce({ chainId, safe })
+  } else if (config == 'override') {
+    return fetchOnChainNonce({ chainId, safe, options })
+  } else {
+    const nonce = config
+    assert(typeof nonce == 'number')
+    return nonce
+  }
+}
+
+async function fetchOnChainNonce({
+  chainId,
+  safe,
+  options,
+}: {
+  chainId: ChainId
+  safe: Address
+  options?: Options
+}): Promise<number> {
+  const provider = getEip1193Provider({ chainId, options })
   const avatarAbi = parseAbi(['function nonce() view returns (uint256)'])
 
-  const nonce = BigInt(
+  const nonce = Number(
     (await provider.request({
       method: 'eth_call',
       params: [
@@ -56,18 +101,30 @@ export async function prepareSafeTransaction({
     })) as string
   )
 
-  return {
-    to: transaction.to,
-    value: transaction.value,
-    data: transaction.data,
-    operation: transaction.operation ?? OperationType.Call,
-    safeTxGas: BigInt(defaults?.safeTxGas || 0),
-    baseGas: BigInt(defaults?.baseGas || 0),
-    gasPrice: BigInt(defaults?.gasPrice || 0),
-    gasToken: getAddress(defaults?.gasToken || zeroAddress) as `0x${string}`,
-    refundReceiver: getAddress(
-      defaults?.refundReceiver || zeroAddress
-    ) as `0x${string}`,
-    nonce: Number(defaults?.nonce || nonce),
+  return nonce
+}
+
+async function fetchQueueNonce({
+  chainId,
+  safe,
+}: {
+  chainId: ChainId
+  safe: Address
+}): Promise<number> {
+  const apiKit = initApiKit(chainId)
+
+  const nonce = await apiKit.getNextNonce(safe)
+
+  return nonce
+}
+
+// TODO: remove this once https://github.com/safe-global/safe-core-sdk/issues/514 is closed
+const initApiKit = (chainId: ChainId): SafeApiKit => {
+  // @ts-expect-error SafeApiKit is only available as a CJS module. That doesn't play super nice with us being ESM.
+  if (SafeApiKit.default) {
+    // @ts-expect-error See above
+    return new SafeApiKit.default({ chainId: BigInt(chainId) })
   }
+
+  return new SafeApiKit({ chainId: BigInt(chainId) })
 }
